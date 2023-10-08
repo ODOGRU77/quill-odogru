d57b0fcbbd917ef1943f0945cd9dd2e5f73cba37
ODOGRU77 <dogruoguzhann@gmail.com>
2023-10-08 23:45:48 +0300

    final tweaks

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import UserAccountNav from "./UserAccountNav";
-// import MobileNav from './MobileNav'
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   const { getUser } = getKindeServerSession();
@@ -22,7 +22,7 @@ const Navbar = () => {
             <span>quill.</span>
           </Link>
 
-          {/* <MobileNav isAuth={!!user} /> */}
+          <MobileNav isAuth={!!user} />
 
           <div className="hidden items-center space-x-4 sm:flex">
             {!user ? (
