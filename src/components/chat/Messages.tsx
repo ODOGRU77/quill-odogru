a721877eee72fbe75385ebc8a6959185d8533610
ODOGRU77 <dogruoguzhann@gmail.com>
2023-10-09 10:27:10 +0300

    final tweaks

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -93,7 +93,7 @@ const Messages = ({ fileId }: MessagesProps) => {
       ) : (
         <div className="flex-1 flex flex-col items-center justify-center gap-2">
           <MessageSquare className="h-8 w-8 text-blue-500" />
-          <h3 className="font-semibold text-xl ">You%apos;re all set!</h3>
+          <h3 className="font-semibold text-xl ">You&apos;re all set!</h3>
           <p className="text-zinc-500 text-sm">
             Ask your first question to get started.
           </p>
